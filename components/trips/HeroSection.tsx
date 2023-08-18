de51fb2dcfaa38cb4d5524f422a7e5d3966f79b2
MadaSosta <ivanmadarcic@gmail.com>
2023-08-18 18:29:10 +0200

    Added city search trips

--- components/trips/HeroSection.tsx
+++ components/trips/HeroSection.tsx
@@ -2,40 +2,42 @@
 
 import { useState, FormEvent } from "react";
 import Link from "next/link";
-
-import { BASE_API_URL } from "@/constants/consts";
-
+import { BASE_API_URL_GEONAME } from "@/constants/consts";
 import { FaSearch } from "react-icons/fa";
 import { MdFastfood, MdBeachAccess } from "react-icons/md";
 import { BiSolidHotel } from "react-icons/bi";
-import { GrHomeOption } from "react-icons/gr";
 import { RxDashboard } from "react-icons/rx";
 
 import styles from "./HeroSection.module.css";
+import { TRadius, TRadiusApiResponse } from "@/types/typings";
+
+type TResponse = {
+  radiusData: TRadiusApiResponse;
+};
 
 function HeroSection() {
   const [activeIndex, setActiveIndex] = useState(0);
   const [searchQuery, setSearchQuery] = useState("");
+  const [data, setData] = useState<TRadius[]>([]);
 
-  const handleSearch = (e: FormEvent<HTMLFormElement>) => {
+  const handleSearch = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const res = fetch(
-      `https://${BASE_API_URL}/v1/reference-data​/locations​/hotels​/by-hotels?hotelIds=ACPAR419`,
-      {
+    const searchParams = new URLSearchParams({ searchQuery });
+
+    try {
+      const res = await fetch("/api/trips?" + searchParams, {
         method: "GET",
-        headers: {
-          Authorization: "Bearer " + process.env.AMADEUS_ACCESS_TOKEN,
-        },
-        mode: "cors",
-      }
-    )
-      .then((res) => {
-        if (res.ok) res.json();
-        else throw new Error("Something went wrong");
-      })
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err));
+      });
+      const { radiusData } = (await res.json()) as TResponse;
+
+      /* Collect all radiuses from returned data */
+      const radiusesArray = radiusData.features.map((item) => item.properties);
+
+      setData(radiusesArray);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
@@ -101,6 +103,25 @@ function HeroSection() {
           <FaSearch className={styles.icon} />
         </button>
       </form>
+      {data.length > 0 && (
+        <div className={styles.resultsWrapper}>
+          <h2>Search results for {searchQuery}</h2>
+          {data.map(
+            (item) =>
+              item.name && (
+                <div className={styles.resultContainer} key={item.xid}>
+                  <h3 className={styles.resultTitle}>{item.name}</h3>
+                  <Link
+                    href={`/trip/${item.xid}`}
+                    className={styles.resultButton}
+                  >
+                    View details
+                  </Link>
+                </div>
+              )
+          )}
+        </div>
+      )}
     </div>
   );
 }
--- components/trips/Index.tsx
+++ components/trips/Index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import styles from "./Trips.module.css";
 import HeroSection from "./HeroSection";
+import FeaturedSection from "./FeaturedSection";
 
 function Trips() {
   return (
     <div className={styles.tripsWrapper}>
       <div className={styles.tripsContainer}>
         <HeroSection />
+        <FeaturedSection />
       </div>
     </div>
   );
