4a1cdd35de241f2187ac0ce2dcc88c468a52d7c1
MadaSosta <ivanmadarcic@gmail.com>
2023-09-04 19:01:57 +0200

    Fixed center issue

--- components/home/AboutSection.tsx
+++ components/home/AboutSection.tsx
@@ -5,62 +5,66 @@ import styles from "./AboutSection.module.css";
 function AboutSection() {
   return (
     <section className={styles.about} id="about">
-      <article className={styles.aboutInner}>
-        <div className={styles.aboutWrapper}>
-          <div className={styles.aboutInfo}>
-            <div className={styles.aboutInfoLeft}>
-              <p className={styles.customHeading}>About Us</p>
-              <p className={cinzelDecorative.className}>
-                EXPERTLY CRAFTED TOURS TO CROATIA. 10 DAYS OF ADMIRING
-                INCREDIBLE NATURE, BEAUTIFUL CITIES, NATIONAL PARKS, AND THE
-                MAGNIFICENT MEDITERIAN SEA.
+      <div className={styles.aboutCenter}>
+        <article className={styles.aboutInner}>
+          <div className={styles.aboutWrapper}>
+            <div className={styles.aboutInfo}>
+              <div className={styles.aboutInfoLeft}>
+                <p className={styles.customHeading}>About Us</p>
+                <p className={cinzelDecorative.className}>
+                  EXPERTLY CRAFTED TOURS TO CROATIA. 10 DAYS OF ADMIRING
+                  INCREDIBLE NATURE, BEAUTIFUL CITIES, NATIONAL PARKS, AND THE
+                  MAGNIFICENT MEDITERIAN SEA.
+                </p>
+              </div>
+
+              <div className={styles.aboutInfoRight}>
+                <p>
+                  We offer guided tours in Croatia - one of the most beautiful
+                  Slavic countries. We will cross the entire country, from
+                  Zagreb to Dubrovnik, visit amazing cities, and see stunning
+                  nature.
+                </p>
+                <p>
+                  The program is carefully thought out, there are no more than
+                  10 people in each group plus an English-speaking guide.
+                  Comfortable vans and nights in 3*+ hotels are included in the
+                  price.
+                </p>
+              </div>
+            </div>
+          </div>
+        </article>
+
+        <article className={styles.contactInfo}>
+          <div className={styles.contactInfoInner}>
+            <div className={`${styles.contact} ${styles.contactCard}`}>
+              <h1>1</h1>
+              <h4 className={cinzelDecorative.className}>Contact us</h4>
+              <p>
+                Some tour conditions may be changed according to the season. To
+                learn more about the program, please contact us.
               </p>
             </div>
-
-            <div className={styles.aboutInfoRight}>
+            <div className={`${styles.date} ${styles.contactCard}`}>
+              <h1>2</h1>
+              <h4 className={cinzelDecorative.className}>Pick a date</h4>
               <p>
-                We offer guided tours in Croatia - one of the most beautiful
-                Slavic countries. We will cross the entire country, from Zagreb
-                to Dubrovnik, visit amazing cities, and see stunning nature.
+                We conduct tours all year round, almost every week, but the most
+                comfortable weather for sightseeing is in summer.
               </p>
+            </div>
+            <div className={`${styles.start} ${styles.contactCard} `}>
+              <h1>3</h1>
+              <h4 className={cinzelDecorative.className}>Start your journey</h4>
               <p>
-                The program is carefully thought out, there are no more than 10
-                people in each group plus an English-speaking guide. Comfortable
-                vans and nights in 3*+ hotels are included in the price.
+                If necessary, we help you apply for a visa to Croatia, as well
+                as advise what things you should take with you.
               </p>
             </div>
           </div>
-        </div>
-      </article>
-
-      <article className={styles.contactInfo}>
-        <div className={styles.contactInfoInner}>
-          <div className={`${styles.contact} ${styles.contactCard}`}>
-            <h1>1</h1>
-            <h4 className={cinzelDecorative.className}>Contact us</h4>
-            <p>
-              Some tour conditions may be changed according to the season. To
-              learn more about the program, please contact us.
-            </p>
-          </div>
-          <div className={`${styles.date} ${styles.contactCard}`}>
-            <h1>2</h1>
-            <h4 className={cinzelDecorative.className}>Pick a date</h4>
-            <p>
-              We conduct tours all year round, almost every week, but the most
-              comfortable weather for sightseeing is in summer.
-            </p>
-          </div>
-          <div className={`${styles.start} ${styles.contactCard} `}>
-            <h1>3</h1>
-            <h4 className={cinzelDecorative.className}>Start your journey</h4>
-            <p>
-              If necessary, we help you apply for a visa to Croatia, as well as
-              advise what things you should take with you.
-            </p>
-          </div>
-        </div>
-      </article>
+        </article>
+      </div>
     </section>
   );
 }
